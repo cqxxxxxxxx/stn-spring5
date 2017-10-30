c618a492a757c90627e070b0de659039ea938ebb
BG307435 <jxchen35@best-inc.com>
2017-10-30 18:41:56 +0800

    Initial commit

---
+++ src/main/java/com/cqx/StnSpring5Application.java
@@ -0,0 +1,12 @@
+package com.cqx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StnSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StnSpring5Application.class, args);
+	}
+}
